0535fd41ad346d7dd17c5adf365f25a3debeb9cc
achint-bansal <65944599+achint-bansal@users.noreply.github.com>
2020-08-05 23:25:11 +0530

    Edit feature on list items

--- src/App.js
+++ src/App.js
@@ -8,26 +8,40 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      inputItem: "",
+      item: {
+        inputItem: "",
+        key: "",
+      },
       list: [],
     };
   }
 
   changeInput(input) {
     this.setState({
-      inputItem: input,
+      item: {
+        inputItem: input,
+        key: Date.now(),
+      },
     });
+    console.log(this.state.item.key);
   }
 
   addItem(input, e) {
     e.preventDefault();
     if (input) {
-      let listArray = this.state.list;
-      listArray.push(input);
-      this.setState({
-        list: listArray,
-        inputItem: "",
-      });
+      // let listArray = this.state.list;
+      // listArray.push(input);
+      const newItem = this.state.item;
+      if (newItem.text !== "") {
+        const list = [...this.state.list, newItem];
+        this.setState({
+          list: list,
+          item: {
+            inputItem: "",
+            key: "",
+          },
+        });
+      }
     }
   }
 
@@ -36,6 +50,19 @@ export default class App extends Component {
     list.splice(itemKey, 1);
     this.setState({ list: list });
   }
+  updateItem(updatedInput, i) {
+    // console.log("key is "+i);
+    const list = this.state.list;
+    list.map((current) => {
+      if (current.key === i) {
+        console.log(current.key);
+        current.inputItem = updatedInput;
+      }
+    });
+    this.setState({
+      list: list,
+    });
+  }
 
   render() {
     return (
@@ -43,23 +70,28 @@ export default class App extends Component {
         <div className="todo-inputbar">
           <form>
             <input
-              onChange={(e) => this.changeInput(e.target.value)}
-              value={this.state.inputItem}
+              value={this.state.item.inputItem}
               placeholder=" + Add Item"
               type="text"
+              onChange={(e) => this.changeInput(e.target.value)}
             />
             <button
               type="submit"
-              onClick={(e) => this.addItem(this.state.inputItem, e)}
+              onClick={(e) => this.addItem(this.state.item.inputItem, e)}
             >
               Add
             </button>
           </form>
         </div>
-        <ul>
+        <ul className="updateInput">
           {this.state.list.map((item, i) => (
-            <li key={i}>
-              {item}
+            <li key={item.key}>
+              {/* {item} */}
+              <input
+                type="text"
+                value={item.inputItem}
+                onChange={(e) => this.updateItem(e.target.value, item.key)}
+              />
               <FontAwesomeIcon
                 icon={faTrashAlt}
                 onClick={() => this.deleteItem(i)}
