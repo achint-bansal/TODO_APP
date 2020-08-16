6280648eb2b7843930b974aca67cb96e925831cf
achint-bansal <65944599+achint-bansal@users.noreply.github.com>
2020-08-16 23:36:59 +0530

    Updates

---
+++ precache-manifest.7391110a99fb378e40f84867035f7a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405d036974b346b58cbfce196ab8f61a",
+    "url": "/TODO_APP/index.html"
+  },
+  {
+    "revision": "6ddb644535fbc863202b",
+    "url": "/TODO_APP/static/css/main.c0d95776.chunk.css"
+  },
+  {
+    "revision": "2124fc9c8728988dd3e9",
+    "url": "/TODO_APP/static/js/2.6e91d9fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TODO_APP/static/js/2.6e91d9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ddb644535fbc863202b",
+    "url": "/TODO_APP/static/js/main.1f4c7877.chunk.js"
+  },
+  {
+    "revision": "231e1242566c0b14556d",
+    "url": "/TODO_APP/static/js/runtime-main.088ad595.js"
+  }
+]);
